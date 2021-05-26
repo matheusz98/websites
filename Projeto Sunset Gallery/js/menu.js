edf6eb45fe484c7df56db27062156025652ae901
matheusz98 <matheuscastiglia31@gmail.com>
2021-05-26 06:01:59 -0300

    Projeto Sunset Gallery

---
+++ Projeto Sunset Gallery/js/menu.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    const menuToggle = document.querySelector('.toggle');
+    const menu = document.querySelector('.menu');
+
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+}
