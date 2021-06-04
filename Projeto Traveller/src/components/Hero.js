82d609e4f008048f0658dfb432c4bb2242452f4d
matheusz98 <matheuscastiglia31@gmail.com>
2021-06-04 19:22:04 -0300

    Componente Hero

---
+++ Projeto Traveller/src/components/Hero.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button } from './Button';
+import './Hero.css';
+import './App.css';
+
+function Hero() {
+    return (
+        <div className="hero-container">
+            <video src="/videos/1.mp4" autoPlay loop muted />
+            <h1>Uma grande aventura o aguarda</h1>
+            <p>Junte-se conosco!</p>
+            <div className="hero-btns">
+                <Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large">
+                    Comece já!                    
+                </Button>
+
+                <Button className="btns" buttonStyle="btn--primary" buttonSize="btn--large">
+                    Nossos trabalhos
+                </Button>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
