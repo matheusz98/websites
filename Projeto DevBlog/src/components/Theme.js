b5f0b33ded045e1c7b8bbc787f7669d3a01a0f96
matheusz98 <matheuscastiglia31@gmail.com>
2021-06-05 23:37:43 -0300

    Estrutura incial do projeto

---
+++ Projeto DevBlog/src/components/Theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const Theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#2c387e",
+      },
+      background: {
+        default: "#1c54b2",
+      },
+    },
+  });
+
+export default Theme;
