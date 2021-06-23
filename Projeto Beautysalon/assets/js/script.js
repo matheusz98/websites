49845390a8b856a006576dde0766704044dcc7c7
matheusz98 <matheuscastiglia31@gmail.com>
2021-06-23 17:48:00 -0300

    Seções de depoimentos e contato

--- Projeto Beautysalon/assets/js/script.js
+++ Projeto Beautysalon/assets/js/script.js
@@ -28,3 +28,30 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scroll');
     }
 });
+
+// Swiper js
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true
+})
+
+// Scroll reveal
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 500,
+    reset: true
+});
+
+scrollReveal.reveal(
+    `#home .image, #home .text, 
+    #about .image, #about .text, 
+    #services header, #services .card, 
+    #testimonials header, #testimonials .testimonials 
+    #contact .text, #contact .links`, 
+    { interval: 100 }
+);
