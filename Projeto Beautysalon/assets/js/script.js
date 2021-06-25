527119497ca7a143314aadf428391a481deaafa0
matheusz98 <matheuscastiglia31@gmail.com>
2021-06-25 03:40:28 -0300

    Responsividade e finalização do projeto

--- Projeto Beautysalon/assets/js/script.js
+++ Projeto Beautysalon/assets/js/script.js
@@ -18,9 +18,9 @@ for (const link of links) {
 }
 
 // Mudar o header da página quando der scroll
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
 function changeHeaderWhenScroll() {
-    const header = document.querySelector('#header');
-    const navHeight = header.offsetHeight;
 
     if(window.scrollY >= navHeight) {
         header.classList.add('scroll');
@@ -36,7 +36,13 @@ const swiper = new Swiper('.swiper-container', {
         el: '.swiper-pagination'
     },
     mousewheel: true,
-    keyboard: true
+    keyboard: true,
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
 })
 
 // Scroll reveal
@@ -58,13 +64,33 @@ scrollReveal.reveal(
 );
 
 // Back to top
+const backToTopButton = document.querySelector('.back-to-top');
 function backToTop() {
-    const backToTopButton = document.querySelector('.back-to-top');
 
-    if (window.scrollY >= 600) {
+    if (window.scrollY >= 560) {
         backToTopButton.classList.add('show');
     } else {
         backToTopButton.classList.remove('show');
+    }
+}
+
+// Link ativo
+const sections = document.querySelectorAll('main section[id]');
+function activateMenuAtCurrentSection() {
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+    for (const section of sections) {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+        const sectionId = section.getAttribute('id');
+        const checkpointStart = checkpoint >= sectionTop;
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+        if (checkpointStart && checkpointEnd) {
+            document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.add('active');
+        } else {
+            document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.remove('active');
+        }
     }
 }
 
@@ -72,4 +98,5 @@ function backToTop() {
 window.addEventListener('scroll', () => {
     changeHeaderWhenScroll();
     backToTop();
-})
+    activateMenuAtCurrentSection();
+});
