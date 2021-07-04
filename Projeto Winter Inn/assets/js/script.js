b2d55e273d0d20c68e3df1cf819dc54344f81727
matheusz98 <matheuscastiglia31@gmail.com>
2021-07-04 16:36:36 -0300

    Footer

--- Projeto Winter Inn/assets/js/script.js
+++ Projeto Winter Inn/assets/js/script.js
@@ -6,19 +6,3 @@ function toggleMenu(){
     menuToggle.classList.toggle('active');
     navLinks.classList.toggle('active');
 }
-
-const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-    },
-    mousewheel: true,
-    keyboard: true,
-    breakpoints: {
-        767: {
-            slidesPerView: 2,
-            setWrapperSize: true
-        }
-    }
-})
