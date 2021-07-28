dac2893f4100b3ac9f993bf2c7ee4f534175778b
matheusz98 <matheuscastiglia31@gmail.com>
2021-07-28 14:49:33 -0300

    Contact

--- Projeto Top Designers/assets/js/main.js
+++ Projeto Top Designers/assets/js/main.js
@@ -14,3 +14,33 @@ $(document).ready(function(){
         },
     },
 });
+
+// Form & Modal
+const form = document.querySelector('.contact-form');
+const name = document.querySelector('.full-name');
+const email = document.querySelector('.email-address');
+const subject = document.querySelector('.subject');
+const message = document.querySelector('.message');
+const submit = document.querySelector('.submit-form');
+const modal = document.querySelector('.modal-wrapper');
+const close = document.querySelector('.modal-close');
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    if(
+        name.value == '' || name.value == undefined && 
+        email.value == '' || email.value == undefined &&
+        subject.value == '' || subject.value == undefined &&
+        message.value == '' || message.value == undefined
+        ) {
+        alert('Please enter the information bellow');
+    } else {
+        form.reset();
+        modal.classList.toggle('none');
+    }
+});
+
+close.addEventListener('click', () => {
+    modal.classList.toggle('none');
+});
