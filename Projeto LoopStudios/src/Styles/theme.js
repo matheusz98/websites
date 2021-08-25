feb0d449ce92505d006e7a6e1506746c12697b7c
Matheus Costa <matheuscastiglia31@gmail.com>
2021-08-25 15:27:47 -0300

    Componente Interactive

---
+++ Projeto LoopStudios/src/Styles/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+    colors: {
+        primaryColor: '#0A1128',
+        secondaryColor: '#DC143C',
+        white: '#FFFFFF',
+    },
+
+    font: {
+        family: {
+            default: "'Open Sans', sans-serif",
+            secondary: "'Montserrat', sans-serif",
+        },
+    },
+};
---
+++ Projeto LoopStudios/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './pages/App/index';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
