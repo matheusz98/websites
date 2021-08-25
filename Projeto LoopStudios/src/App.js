0833c6f1bdd2abc888a84bf5f392b39ac2394d7b
Matheus Costa <matheuscastiglia31@gmail.com>
2021-08-25 14:19:51 -0300

    Estrutura inicial e componente Header

---
+++ Projeto LoopStudios/src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header/Header";
+import "./Styles/App/App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ Projeto LoopStudios/src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import "../../Styles/Header/Header.css";
+import logo from "../../images/logo.svg";
+
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <div className="logo">
+          <img src={logo} alt="LoopStudios logo" />
+        </div>
+
+        <nav>
+          <ul>
+            <li>
+              <button>About</button>
+            </li>
+            <li>
+              <button>Careers</button>
+            </li>
+            <li>
+              <button>Events</button>
+            </li>
+            <li>
+              <button>Products</button>
+            </li>
+            <li>
+              <button>Support</button>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="menu-btn">
+          <div className="bar bar-1"></div>
+          <div className="bar bar-2"></div>
+          <div className="bar bar-3"></div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
