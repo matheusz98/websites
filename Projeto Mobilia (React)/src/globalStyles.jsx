6bd7621677140a165b3ad06ac4dea163ae5db471
Matheus Costa <matheuscastiglia31@gmail.com>
2021-09-28 11:30:11 -0300

    Update globalStyles.jsx

--- Projeto Mobilia (React)/src/globalStyles.jsx
+++ Projeto Mobilia (React)/src/globalStyles.jsx
@@ -11,7 +11,6 @@ export const GlobalStyle = createGlobalStyle`
         outline: none;
         font-family: "Merriweather", serif;
         font-weight: 400;
-        overflow-x: hidden;
     }
     
     img {
@@ -20,6 +19,7 @@ export const GlobalStyle = createGlobalStyle`
 
     body {
         background: #f1e7e7;
+        overflow-x: hidden;
     }
 
     p {
