bfabe9d1b01ad5eb3d22ac9dd19458fef8e7174b
Matheus Costa <matheuscastiglia31@gmail.com>
2021-11-05 08:48:29 -0300

    Configuração inicial

---
+++ Projeto Twitter Clone/src/App.tsx
@@ -0,0 +1,12 @@
+import GlobalStyles from "./GlobalStyles";
+
+function App() {
+  return (
+    <div>
+      <GlobalStyles />
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
